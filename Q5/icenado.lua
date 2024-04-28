0f41e01bf1510b95a3cfea86f77b7f016a93f689
Rohan <rohanalmeida@live.com>
2024-04-28 17:59:57 +0530

    edited the folowing files from their respective directories. icenado.lua is brand new file added to data\spells\scripts\attack

---
+++ Q5/icenado.lua
@@ -0,0 +1,17 @@
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
+
+combat:setArea(createCombatArea(AREA_CIRCLE3X3))
+
+function onGetFormulaValues(player, level, magicLevel)
+	local min = (level / 5) + (magicLevel * 0.8) + 5
+	local max = (level / 5) + (magicLevel * 2) + 12
+	return -min, -max
+end
+
+combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+
+function onCastSpell(creature, variant)
+	return combat:execute(creature, variant)
+end
